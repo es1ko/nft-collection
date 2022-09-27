f7189bb9c57ef2eaae70649a6256799ca9020273
es1ko <80271765+es1ko@users.noreply.github.com>
2022-09-27 16:19:21 +0600

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xcBC9400df0c33d829b817545d2a09bc1eAd2B90C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
